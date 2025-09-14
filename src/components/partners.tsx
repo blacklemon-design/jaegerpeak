52544d7c8841d63118b72a9685afeb2e6456b348
Hiebeler <emanuel.hiebeler@gmail.com>
2025-09-14 15:01:27 +0200

    add grey tc widnau logo

--- src/components/partners.tsx
+++ src/components/partners.tsx
@@ -1,24 +1,23 @@
-import { Award, Users, Target, Zap } from "lucide-react"
-import { Card, CardContent } from "@/components/ui/card"
+import { Award, Target, Users, Zap } from "lucide-react";
+import { Card, CardContent } from "@/components/ui/card";
 
 export function Partners() {
     return (
         <section id="about" className="py-20 bg-background-secondary">
             <div className="container mx-auto px-4 sm:px-6 lg:px-8 mb-8">
                 <div className="text-center space-y-4 mb-16">
-          <h2 className="text-3xl md:text-4xl font-bold">
-            Partner
-          </h2>
-        </div>
+                    <h2 className="text-3xl md:text-4xl font-bold">
+                        Partner
+                    </h2>
+                </div>
                 <div className="grid lg:grid-cols-3 gap-12 items-center">
-
                     <img
-                        src="/images/tc-widnau.png"
+                        src="/images/tc-widnau-grey.png"
                         alt="Athletic fitness coach in dynamic jumping pose"
                         className="w-auto max-h-60 obeject-cover mx-auto"
                     />
                     <img
-                        src="/images/tc-widnau.png"
+                        src="/images/tc-widnau-grey.webp"
                         alt="Athletic fitness coach in dynamic jumping pose"
                         className="w-auto max-h-60 obeject-cover mx-auto"
                     />
@@ -30,5 +29,5 @@ export function Partners() {
                 </div>
             </div>
         </section>
-    )
+    );
 }
