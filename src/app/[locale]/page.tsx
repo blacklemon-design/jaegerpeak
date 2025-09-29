ced6da8f129f11b91e8026967d0b07d8675588f3
Hiebeler <emanuel.hiebeler@gmail.com>
2025-09-29 22:48:25 +0200

    add intl

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,19 +4,30 @@ import Faq from "@/components/faq";
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
 import { Hero } from "@/components/hero";
+import LocaleSwitcher from "@/components/LocaleSwitcher";
 import { Partners } from "@/components/partners";
 import { Services } from "@/components/services";
 import { Testimonials } from "@/components/testimonials";
 import { loadHomeBannerContent } from "@/lib/utils";
+import { Locale } from "next-intl";
+import { getTranslations } from "next-intl/server";
 export const dynamic = "force-static"; // forces static generation
+
 const data = loadHomeBannerContent(); // runs at build time, before component is even called
-export default async function Home() {
- 
+
+export default async function Home(props: {
+  params: Promise<{ lang: Locale }>;
+}) {
+  const { lang } = await props.params;
+    const t = await getTranslations({ namespace: "Hero", locale: lang });
+
   return (
     <div className="min-h-screen bg-background text-white">
       <Header bannerTitle={data.title} bannerDate={data.date} />
       <main>
+        <h1 className="text-4xl">{t("title")}</h1>
         <Hero />
+        <LocaleSwitcher />
         <About />
         <Services />
         {/** <Testimonials />*/}
