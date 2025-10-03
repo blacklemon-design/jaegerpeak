78f08822decca24f6502845239119153dc6cea44
Hiebeler <emanuel.hiebeler@gmail.com>
2025-10-03 08:08:20 +0200

    fix locale switcher

--- src/components/LocaleSwitcher.tsx
+++ src/components/LocaleSwitcher.tsx
@@ -1,30 +1,23 @@
 "use client";
 
-import { Locale } from "next-intl";
+import { Locale, useLocale } from "next-intl";
 import { getTranslations } from "next-intl/server";
-import { useRouter, usePathname } from "../i18n/navigation";
+import { useRouter, usePathname, Link } from "../i18n/navigation";
 import Flag from "react-world-flags";
 import { useEffect } from "react";
 import { useParams } from "next/navigation";
 
 const LocaleSwitcher = ({ lang }: { lang: Locale }) => {
-  const router = useRouter();
   const pathname = usePathname();
-  const params = useParams<{ locale: string }>()
-  
-  const handleLocaleChange = (newLocale: string) => {
-    // `pathname` is localized, e.g., /en/about or /de/kontakt
-    router.replace(pathname, { locale: newLocale, scroll: false });
-  };
-
+  const locale = useLocale();
   return (
     <div className="flex space-x-4 items-center">
-      <button type="button" onClick={() => handleLocaleChange("de")}>
-        <Flag code="DE" style={{ height: 24 }} className={`rounded-xs ${params.locale === "de" ? "opacity-100": "opacity-50"}`} />
-      </button>
-      <button type="button" onClick={() => handleLocaleChange("en")}>
-        <Flag code="GB" style={{ height: 24 }} className={`rounded-xs ${params.locale === "en" ? "opacity-100": "opacity-50"}`} />
-      </button>
+      <Link href={pathname} locale="de" >
+        <Flag code="DE" style={{ height: 24 }} className={`rounded-xs ${locale === "de" ? "opacity-100": "opacity-50"}`} />
+      </Link>
+      <Link href={pathname} locale="en">
+        <Flag code="GB" style={{ height: 24 }} className={`rounded-xs ${locale === "en" ? "opacity-100": "opacity-50"}`} />
+      </Link>
     </div>
   );
 };
