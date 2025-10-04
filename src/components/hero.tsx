1075fd5cef8e37ef9081d11bd8b25e9442764d31
Hiebeler <emanuel.hiebeler@gmail.com>
2025-10-04 12:43:18 +0200

    redesign mobile hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -10,26 +10,12 @@ export async function Hero({ lang }: { lang: Locale }) {
   return (
     <section
       id="home"
-      className="pt-16 min-h-screen flex items-center bg-gradient-to-br bg-background mt-14 lg:mt-0 relative"
-    >
-      {/* Hintergrund-Bild für mobile Geräte */}
-      <div className="absolute inset-0 z-0 md:hidden flex items-center justify-center">
-        <div className="relative w-96 h-[28rem] sm:w-[28rem] sm:h-[32rem]">
-          <Image
-            src={"/images/ramon-hero_cut.webp"}
-            alt="Athletic fitness coach in dynamic jumping pose"
-            fill
-            className="object-contain opacity-30"
-            priority
-          />
-        </div>
-        <div className="absolute inset-0 bg-gradient-to-b from-black/20 to-transparent"></div>
-      </div>
-      
+      className="pt-16 md:min-h-screen mb-16 md:mb-0 flex md:items-center bg-gradient-to-br bg-background mt-14 lg:mt-0 relative"
+    > 
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
-        <div className="grid grid-cols-2 gap-0 md:gap-12">
+        <div className="grid grid-cols-2 gap-10 md:gap-12">
           {/* Text-Bereich */}
-          <div className="space-y-6 w-full lg:col-span-1 col-span-2 pt-8 md:pt-0">
+          <div className="space-y-6 w-full lg:col-span-1 col-span-2 justify-center flex flex-col">
             <div className="space-y-4">
               <h1 className="text-4xl md:text-6xl font-bold leading-tight break-normal sm:reverse">
                 Fitness,{" "}
@@ -50,7 +36,7 @@ export async function Hero({ lang }: { lang: Locale }) {
                 </Button>
               </Link>
             </div>
-            <div className="grid-cols-3 gap-6 pt-4 grid">
+            <div className="grid-cols-3 gap-6 pt-4 md:grid hidden">
               <div className="text-center">
                 <div className="text-3xl font-bold text-primary">{t("hpoint1_1")}</div>
                 <div className="text-foreground text-xl">{t("hpoint1_2")}</div>
@@ -67,7 +53,7 @@ export async function Hero({ lang }: { lang: Locale }) {
           </div>
 
           {/* Bild für Desktop-Geräte */}
-          <div className="relative lg:col-span-1 col-span-2 max-h-160 lg:h-full h-90 sm:h-120 md:h-140 w-full md:w-1/2 lg:w-full mx-auto hidden md:block">
+          <div className="relative lg:col-span-1 col-span-2 max-h-160 lg:h-full xl:h-[600px] h-90 sm:h-120 md:h-140 w-full md:w-1/2 lg:w-full mx-auto">
             <Image
               src={"/images/ramon-hero_cut.webp"}
               alt="Athletic fitness coach in dynamic jumping pose"
