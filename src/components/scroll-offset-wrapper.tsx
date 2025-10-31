9bd19c477641086e0dbe363db46b3d6e0d7b58a5
KGahan <kimgahan05@gmail.com>
2025-10-31 16:45:37 +0100

    timeout increased

--- src/components/scroll-offset-wrapper.tsx
+++ src/components/scroll-offset-wrapper.tsx
@@ -31,7 +31,7 @@ export function ScrollOffsetWrapper({
                         behavior: "instant",
                     })
                 }
-            }, 100)
+            }, 500)
         }
     })
 
